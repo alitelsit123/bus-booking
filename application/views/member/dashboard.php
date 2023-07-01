d21cf0ddb3a60397ca22995edf9d056afd912438
kobo kanaeru <reza.eduwork@gmail.com>
2023-07-01 23:43:38 +0700

    update

--- application/views/member/dashboard.php
+++ application/views/member/dashboard.php
@@ -22,6 +22,28 @@
           
         </div>
         <!-- /.row -->
+				<hr />
+				<div class="row">
+					<?php foreach($this->Bus_model->getAll() as $row): ?>
+						<div class="col-4">
+							<div class="card" style="width: 18rem;">
+								<?php if($row->image): ?>
+								<img src="<?= base_url('assets/upload/'.$row->image) ?>" alt="" class="img-card-top" srcset="" style="" />
+								<?php else: ?>
+								Gambar belum di upload
+								<?php endif; ?>
+								<div class="card-body">
+									<h5 class="mb-2"><strong><?= $row->name ?></strong></h5>
+									<div class="d-block">
+										<div class="badge badge-info"><?= $row->plat ?></div>
+										<div class="badge badge-info"><?= $this->Type_model->find($row->type_id)->name ?? '<small><i>Tipe tidak ada atau dibapus!</i></small>' ?></div>
+									</div>
+									<!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+								</div>
+							</div>
+						</div>
+					<?php endforeach; ?>
+				</div>
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
