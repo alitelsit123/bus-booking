00c96bd7de3568c74a79d5729527f5ba88ca6db7
kobo kanaeru <reza.eduwork@gmail.com>
2023-07-28 10:32:25 +0700

    dev

--- application/views/member/book.php
+++ application/views/member/book.php
@@ -23,10 +23,70 @@
 						<p class="card-text">
 							<?= strlen($row->description) > 100 ? substr($row->description, 0, 100).'...':$row->description ?>
 						</p>
-						<a
-						data-toggle="modal"
-						href="#exampleModal<?= $row->id ?>" 
-						class="btn btn-primary btn-block">Sewa Sekarang</a>
+						<div class="d-flex align-items-center justify-content-center">
+							<a href="#d<?= $row->id ?>" data-toggle="modal" class="btn btn-default w-full flex-grow-1 mr-1">Detail Bus</a>
+							<a
+							data-toggle="modal"
+							href="#exampleModal<?= $row->id ?>" 
+							class="btn btn-primary w-full flex-grow-1 ml-1">Sewa Sekarang</a>
+						</div>
+						<!-- Modal -->
+						<div class="modal fade" id="d<?= $row->id ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="exampleModalLabel"><?= $row->name ?></h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body">
+										<?php if($row->image): ?>
+										<img src="<?= base_url('assets/upload/'.$row->image) ?>" alt="" srcset="" 
+										style="width:100%;height:230px;object-fit:contain;"
+										class="card-img-top" />
+										<?php else: ?>
+										Gambar belum di upload
+										<?php endif; ?>
+										<table class="table table-borderless">
+											<tbody>
+												<tr>
+													<td>Nama Bus</td>
+													<td><?= $row->name ?></td>
+												</tr>
+												<tr>
+													<td>Plat</td>
+													<td><?= $row->plat ?></td>
+												</tr>
+												<tr>
+													<td>Tipe</td>
+													<td><?= $this->Type_model->find($row->type_id)->name ?></td>
+												</tr>
+												<tr>
+													<td>Tahun</td>
+													<td><?= $row->year ?></td>
+												</tr>
+												<tr>
+													<td>Jumlah Tempat Duduk</td>
+													<td><?= $row->seat ?></td>
+												</tr>
+												<tr>
+													<td>Harga</td>
+													<td><?= $row->price_daily ?></td>
+												</tr>
+												<tr>
+													<td colspan="2"><?= $row->description ?></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+										<button type="button" class="btn btn-primary">Save changes</button>
+									</div>
+								</div>
+							</div>
+						</div>
 						<!-- Modal -->
 						<div class="modal fade" id="exampleModal<?= $row->id ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 							<div class="modal-dialog" role="document">
